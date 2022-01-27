1d29094fd37c6ae03d9356d5e0bfa8e91dc240be
Jessér Ramiro Ferreira <97418530+jesserferreira@users.noreply.github.com>
2022-01-27 15:55:40 -0300

    atualização css

--- js/painel.js
+++ js/painel.js
@@ -3,14 +3,19 @@ const setaAvancar = document.getElementById('btn-avancar');
 const setaVoltar = document.getElementById('btn-voltar');
 let imagemAtual = 0;
 
-
-
 setaAvancar.addEventListener('click', function() {
     imagemAtual++;
     
-    imagemAtual.array.forEach(imagem => {
-       imagem.classList.remove('mostrar')
-    });;
+    imagensPainel.array.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+    });
+})
+
+
+
+
+    
+/*     imagem.classList.remove('mostrar')
 
     imagemPainel[0]
-})
+*/
